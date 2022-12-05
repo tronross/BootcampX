54360c4a98d7360b60d13ef8a8cbd2cd4210f0f8
tronross <john_ross@analogforever.ca>
2022-12-05 14:00:38 -0500

    Add query cohort_assistance_duration_total

---
+++ 4_queries/cohort_assistance_duration_total.sql
@@ -0,0 +1,10 @@
+--//////////////////////////////////////////////
+-- Total Time Spent on Assistance per Cohort 
+--//////////////////////////////////////////////
+
+SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_duration;
