32a8dfac63a769bbe29d1ab5fb653fb6bae1aa1c
tronross <john_ross@analogforever.ca>
2022-12-05 15:24:06 -0500

    Add query teacher_count_assistances_by_cohort

---
+++ 4_queries/teacher_count_assistances_by_cohort.sql
@@ -0,0 +1,12 @@
+--/////////////////////////////////////////////////////////////////////////////////
+--Teachers Who Performed Assistance Requests and their Total Number of (by Cohort)
+--/////////////////////////////////////////////////////////////////////////////////
+
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name; 
