61f1b0a2638d6b16fbd1ae2a83e01476a8d7dad8
tronross <john_ross@analogforever.ca>
2022-12-05 13:29:58 -0500

    Edit comments

--- 4_queries/assistance_requests_data.sql
+++ 4_queries/assistance_requests_data.sql
@@ -6,6 +6,6 @@ SELECT teachers.name as teacher, students.name as student, assignments.name as a
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
--- capture assistance_requests where assignments.id is null
+-- capture assistance_requests, including where assignments.id is null
 FULL OUTER JOIN assignments ON assignments.id = assignment_id
 ORDER BY duration;
