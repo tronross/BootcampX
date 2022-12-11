050d45810d0c948c491b91ea11c11d4ac29ff160
tronross <john_ross@analogforever.ca>
2022-12-10 22:12:35 -0500

    Lint students

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -14,8 +14,8 @@ JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE '${process.argv[2]}%'
 LIMIT ${process.argv[3] || 5};
 `)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-});
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  });
