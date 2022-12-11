12f41c6ba0ffcce4787216fdb5899f19552e6c93
tronross <john_ross@analogforever.ca>
2022-12-10 23:09:14 -0500

    Fix teachers.js: parameterize query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${process.argv[2]}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2];
+const values =[`${cohortName}`];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(assist => {
       console.log(`${assist.cohort}:  ${assist.teacher}`);
